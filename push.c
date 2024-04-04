6e59f8993f195de756a1bb75d9d19dfd9287af7c
Kiko Sprague <ksprague0592@gmail.com>
2024-04-04 03:02:22 +0000

    monty header w/ strucks and prototypes; starting push function

---
+++ push.c
@@ -0,0 +1,51 @@
+#include "monty.h"
+
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+void push(stack_t **stack, unsigned int line_number);
+{
+	stack_t *new_stack;
+
+	if (_isdigit(line_number == 1))
+	{
+		perror(" ");
+		exit(EXIT_FAILURE);
+	}
+		new_stack = malloc(sizeof(stack_t));
+		
+		if (new_stack == NULL)
+		{
+			perror("Error: Malloc Failed");
+			Exit(EXIT_FAILURE);
+		}
+
+		if (*stack == NULL)
+		{
+
+		new_stack->n = line_number;
+		new_stack->next = NULL;
+		new_stack->prev = NULL;
+		*stack = new_stack;
+		exit(EXIT_SUCCESS);
+		
+		}
+
+		else
+		{
+			new_stack->prev = NULL;
+			new_stack->next = *stack;
+			new_stack->n = line_number;
+			(*head)->prev = new_stack;
+			*head = new_stack;
+			exit(EXIT_SUCCESS);
+		}
+
+}
